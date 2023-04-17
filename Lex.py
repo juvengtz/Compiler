85fccd80b7165ff6a4823e1f7407197a6dcbe104
JG <61897532+a01250658@users.noreply.github.com>
2023-04-16 21:36:22 -0600

    LexYacc

---
+++ Lex.py
@@ -0,0 +1,80 @@
+import ply.lex as lex
+# KEYWORDS
+keywords = {
+    'program': 'PROGRAM',
+    'var': 'VAR',
+    'func': 'FUNC',
+    'main': 'MAIN',
+    'int': 'INT',
+    'float': 'FLOAT',
+    'string': 'STRING',
+    'while': 'WHILE',
+    'for': 'FOR',
+    'if': 'IF',
+    'else': 'ELSE',
+    'print': 'PRINT',
+}
+# TOKENS
+tokens = [
+    'ID', 'SEMICOLON', 'COMA', 'COLON',
+    'BRACKET_L', 'BRACKET_R', 'EQUAL', 'NOT_EQUAL', 'ASSIGN',
+    'PARENTESIS_L', 'PARENTESIS_R', 'CURLY_L', 'CURLY_R',
+    'CTE_STRING', 'PLUS', 'MINUS', 'MULT', 'DIV', 'MORE', 'LESS',
+    'CTE_INT', 'CTE_FLOAT'
+] + list(keywords.values())
+# REGEX
+t_SEMICOLON = r'\;'
+t_COMA = r'\,'
+t_COLON = r'\:'
+t_BRACKET_L = r'\['
+t_BRACKET_R = r'\]'
+t_CURLY_L = r'\{'
+t_CURLY_R = r'\}'
+t_ASSIGN = r'\='
+t_EQUAL = r'\=\='
+t_NOTEQUAL = r'\!\='
+t_PARENTESIS_L = r'\('
+t_PARENTESIS_R = r'\)'
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_MULT = r'\*'
+t_DIV = r'\/'
+t_MORE = r'\>'
+t_LESS = r'\<'
+t_ignore = ' \t'
+
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    return t
+
+
+def t_CTE_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_CTE_FLOAT(t):
+    r'[0-9]*\.[0-9]+'
+    t.value = float(t.value)
+    return t
+
+
+def t_CTE_STRING(t):
+    r'\"(\\.|[^"\\])*\"'
+    t.value = str(t.value)
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
---
+++ Yacc.py
@@ -0,0 +1,157 @@
+from Lex import tokens
+import ply.yacc as yacc
+import sys
+
+# GRAMMAR
+
+
+def p_programa(p):
+    '''programa : PROGRAM PARENTESIS_L PARENTESIS_R bloque'''
+
+
+def p_bloque(p):
+    '''bloque : CURLY_L var1 estatuto1 CURLY_L'''
+
+
+def p_var1(p):
+    '''var1 : var2
+            | empty'''
+
+
+def p_var2(p):
+    '''var2 : VAR var3
+            | var2 var2'''
+
+
+def p_var3(p):
+    '''var3 : var4 COLON tipo SEMICOLON'''
+
+
+def p_var4(p):
+    '''var4 : ID
+              | var4 COMA var4'''
+
+
+def p_tipo(p):
+    '''tipo : INT
+            | FLOAT
+            | STRING'''
+
+
+def p_estatuto1(p):
+    '''estatuto1 : estatuto2 SEMICOLON
+                | estatuto1 estatuto1'''
+
+
+def p_estatuto2(p):
+    '''estatuto2 : asignacion 
+                | condicion
+                | escritura
+                | whileLoop
+                | forLoop'''
+
+
+def p_asignacion(p):
+    '''asignacion : ID ASSIGN expresion'''
+
+
+def p_escritura(p):
+    '''escritura : PRINT PARENTESIS_L escritura_1 PARENTESIS_R'''
+
+
+def p_escritura_1(p):
+    '''escritura_1 : escritura_2
+                    | escritura_2 COMA escritura_2'''
+
+
+def p_escritura_2(p):
+    '''escritura_2 : STRING
+                    | expresion'''
+
+
+def p_expresion(p):
+    '''expresion : exp
+    | exp MORE exp
+    | exp LESS exp
+    | exp EQUAL exp
+    | exp NOT_EQUAL exp'''
+
+
+def p_whileLoop(p):
+    '''whileLoop : WHILE PARENTESIS_L expresion PARENTESIS_R bloque'''
+
+
+def p_forLoop(p):
+    '''forLoop : FOR PARENTESIS_L argumento expresion SEMICOLON expresion PARENTESIS_R bloque'''
+
+
+def p_condicion(p):
+    '''condicion : IF PARENTESIS_L expresion PARENTESIS_R bloque condicion2'''
+
+
+def p_condicion2(p):
+    '''condicion2 : ELSE bloque
+                     | empty'''
+
+
+def p_argumento(p):
+    '''argumento : argumento2
+                  | empty'''
+
+
+def p_argumento2(p):
+    '''argumento2 : ID COLON tipo
+            | argumento2 COMA argumento2'''
+
+
+def p_exp(p):
+    '''exp : termino
+           | termino signo'''
+
+
+def p_signo(p):
+    '''signo : PLUS
+             | MINUS'''
+
+
+def p_termino(p):
+    '''termino : factor
+               | factor operacion'''
+
+
+def p_operacion(p):
+    '''operacion : MULT exp
+                 | DIV exp'''
+
+
+def p_factor(p):
+    '''factor : PARENTESIS_L expresion PARENTESIS_R
+               | var_cte'''
+
+
+def p_var_cte(p):
+    '''var_cte : ID 
+              | CTE_INT
+              | CTE_FLOAT'''
+
+
+def p_empty(p):
+    '''empty :'''
+
+
+def p_error(p):
+    print("ERROR type Syntax", p)
+
+
+parser = yacc.yacc()
+
+filename = input("file name: ")
+
+
+try:
+    f = open(filename, 'r')
+    data = f.read()
+    f.close()
+    parser.parse(data)
+except:
+    print("ERROR File")
