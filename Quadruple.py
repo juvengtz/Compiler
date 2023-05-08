08ee907dee3b0a3c401db7d32ccaf9320ec6f24f
JG <61897532+a01250658@users.noreply.github.com>
2023-05-07 23:05:55 -0600

    Quadruple Fix

--- Quadruple.py
+++ Quadruple.py
@@ -9,8 +9,36 @@ class Quadruple:
         self.result = None
 
     def create_quadruple(self, operator, left_operand, right_operand, result):
+        if left_operand.type not in SEMANTIC_CUBE[operator][right_operand.type]:
+            raise TypeError(
+                f"Invalid operation: {left_operand.type} {operator} {right_operand.type}")
+
         self.operator = operator
         self.left_operand = left_operand
         self.right_operand = right_operand
         self.result = result
         return self
+
+    def get_operator(self):
+        return self.operator
+
+    def set_operator(self, operator):
+        self.operator = operator
+
+    def get_left_operand(self):
+        return self.left_operand
+
+    def set_left_operand(self, left_operand):
+        self.left_operand = left_operand
+
+    def get_right_operand(self):
+        return self.right_operand
+
+    def set_right_operand(self, right_operand):
+        self.right_operand = right_operand
+
+    def get_result(self):
+        return self.result
+
+    def set_result(self, result):
+        self.result = result
--- README.md
+++ README.md
@@ -5,7 +5,9 @@ This is our project made with PLY, which is a reimplementation of the classical 
 So far the lexer and parser have an initial implementation, that allows them to recognize specific patterns in their input.
 - Lex.py contains the lexer/scanner: A module to convert characters (user input) into tokens which are fed to the parser
 - Yacc.py contains the parser: A module to convert tokens into a specialized tree data structure.
-
+- SemanticCube.py contains the semantic cube, which is a data structure that stores the type compatibility of different operators in a programming language. 
+In this case, the cube is defined for the types "INT", "FLOAT", and "STRING", and for each type, it specifies which operators are compatible with which other types.
+- Quadruple.py contains a data structure used in compiler construction to store information about operations to be performed on operands. It contains information such as the operator, left and right operands, and the result
 ## Authors
 - Yannin Flores Maza A01324745
 - Juventino Gutierrez Romo A01250658
